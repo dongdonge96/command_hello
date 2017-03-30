86d0ca4b06735391b07b6170f513c8d0958cadb8
donghee <dongodnge96@naver.com>
2017-03-30 11:11:55 +0900

    conflict resolved GitHub

---
+++ hello.py
@@ -0,0 +1,7 @@
+//For command line git tutorial
+//First: GitHub modification
+//Second: Local repository modification
+print("Hello world")
+print("Tell your world")
+print("Tell his world")
+print("Tell her world")
