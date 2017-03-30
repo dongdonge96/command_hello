427cc2e6420440d71488a062b7208fe068dd55f8
dongdonge96 <dongdonge96@users.noreply.github.com>
2017-03-30 11:30:43 +0900

    GitHub for git pull

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 //For command line git tutorial
-//Local respository modification
+//First: GitHub modification
+//Second: Local respository modification
 print("Hello world")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+print("Tell my world")
